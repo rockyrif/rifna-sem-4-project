e6a04e955d468fc11823584074a999bd69701c4a
rockyrif <mnamrifky@gmail.com>
2024-03-25 11:58:06 +0530

    first

--- page/admin-dashbord/table/admin-dashbord.php
+++ page/admin-dashbord/table/admin-dashbord.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit members</title>
+    <title>Edit items</title>
 
 
 
